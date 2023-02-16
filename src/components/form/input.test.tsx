7ecdd5ecd78bd91200c61c9ee4eff42b5605bf38
Caricati <mrcaricati@proton.me>
2023-02-16 17:47:47 -0300

    improve the input tests

--- src/components/form/input.test.tsx
+++ src/components/form/input.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '../../testing'
+import { fireEvent, render } from '../../testing'
 
 import Input from './input'
 
@@ -6,5 +6,66 @@ describe('<Input />', () => {
   test('renders a input correctly', () => {
     const { container } = render(<Input type="text" value="" />)
     expect(container.querySelector('input')).toBeInTheDocument()
+  })
+
+  test('dispatchs an event when the user press the keyboard', () => {
+    const fn = jest.fn()
+    const { container } = render(<Input type="text" value="" onChange={fn} />)
+    const input = container.querySelector('input') as HTMLElement
+
+    fireEvent.change(input, { target: { value: 'a' } })
+
+    expect(fn).toBeCalledTimes(1)
+  })
+
+  test('renders an icon on the left of the Input', () => {
+    const { container } = render(<Input type="text" value="" leftIcon="bell" />)
+    const svg = container.querySelector('svg')
+
+    expect(svg).toBeInTheDocument()
+  })
+
+  test('renders a button on the right of the input', () => {
+    const fn = jest.fn()
+    const { container, getByRole } = render(
+      <Input
+        type="text"
+        value=""
+        leftIcon="bell"
+        button={{
+          icon: 'caret-down',
+          onClick() {
+            fn()
+          },
+        }}
+      />
+    )
+
+    const svg = container.querySelector('svg')
+    const button = getByRole('button')
+    fireEvent.click(button)
+
+    expect(svg).toBeInTheDocument()
+    expect(fn).toBeCalledTimes(1)
+  })
+
+  test('renders a label to the input', () => {
+    const fn = jest.fn()
+    const { getByText } = render(
+      <Input
+        type="text"
+        value=""
+        label="my label"
+        leftIcon="bell"
+        button={{
+          icon: 'caret-down',
+          onClick() {
+            fn()
+          },
+        }}
+      />
+    )
+
+    expect(getByText('my label')).toBeInTheDocument()
   })
 })
